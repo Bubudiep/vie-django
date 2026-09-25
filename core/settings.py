08df884b04309bd57936d7f3f9b69217504e5be3
bubu_diep <hlitdvp98@gmail.com>
2026-09-25 09:19:51 +0700

    update static

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,10 @@ TIME_ZONE = 'Asia/Ho_Chi_Minh'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MAILERS = {
@@ -224,4 +228,3 @@ LOGGING = {
         },
     },
 }
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # hoặc os.path.join(BASE_DIR, 'static')
